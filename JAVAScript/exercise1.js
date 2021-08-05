ac1b2af7be0971ab56c63b864aabe06d44498270
vaibhavgupta942466 <vaibhavgupta942466@gmail.com>
2021-08-05 17:50:54 +0530

    Java Script

---
+++ JAVAScript/exercise1.js
@@ -0,0 +1,3 @@
+var tamp = prompt("What is the weight in pounds (Ibs)?");
+alert("This is: " +(tamp*0.454) +" Kilogram");
+console.log("Conversion Completed");
---
+++ JAVAScript/myscript.js
@@ -0,0 +1,23 @@
+// alert("Welcome to my world");
+// var tamp=prompt("How much money u have");
+// alert("So u have: "+tamp);
+// console.log("You are a cool person");
+// var hot = false;
+// var temp = 49;
+
+// if (temp>80) {
+//     console.log("Temp is greater than 80");
+//     hot=true;
+// }else{
+//     console.log("Its not very hot today");
+// }
+// console.log(hot);
+// if (temp>80) {
+//     console.log("Hot Outside!");
+// }else if (temp <= 80 && temp >=50) {
+//     console.log("Average temp Outside");
+// }else if (temp<50 && temp>=32) {
+//     console.log("Its Pretty cold out");
+// }else{
+//     console.log("Its very cold oput");
+// }
