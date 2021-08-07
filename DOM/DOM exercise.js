18af2f2ba0844d095e052c9318b3031732ea3cb8
vaibhavgupta942466 <vaibhavgupta942466@gmail.com>
2021-08-07 18:51:05 +0530

    Game Completed

--- DOM/DOM exercise.js
+++ DOM/DOM exercise.js
@@ -1,7 +1,7 @@
 //Restart game Button
 var restart = document.querySelector("#custom_button");
 //Grab all The Squares
-var squares = document.querySelectorAll("#ram");
+var squares = document.querySelectorAll(".ram");
 //clear all the squares
 
 function clearboard(){
@@ -10,4 +10,28 @@ function clearboard(){
     }
 }
 
-console.log(squares.length)
+restart.addEventListener("click", clearboard);
+
+// var cellone = document.querySelector('#a')
+// cellone.addEventListener('click',function(){
+//     if(cellone.textContent === " "){
+//         cellone.textContent = "X";
+//     }else if(cellone.textContent === "X"){
+//         cellone.textContent = "0";
+//     }else{
+//         cellone.textContent = " ";
+//     }
+// })
+
+function changemarker(){
+    if(this.textContent === " "){
+        this.textContent = "X";
+    }else if(this.textContent === "X"){
+        this.textContent = "0";
+    }else{
+        this.textContent = " ";
+    }
+}
+for(var i = 0 ; i < squares.length ;i++){
+    squares[i].addEventListener("click",changemarker)
+}
