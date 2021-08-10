1ce1895a4f4e6d8d9cd35da27d2c8d18faaec7b3
vaibhavgupta942466 <vaibhavgupta942466@gmail.com>
2021-08-10 18:09:02 +0530

    testign

--- python/Basic_python.py
+++ python/Basic_python.py
@@ -1,43 +1,74 @@
-print("Hello World")
-print(2+10*10+3)
-a=5
-print(a+a)
-a=a*5
-print(a)
+# print("Hello World")
+# print(2+10*10+3)
+# a=5
+# print(a+a)
+# a=a*5
+# print(a)
 
-my_income = 100
-tax_rate = 0.1 # 10%
-my_taxes = my_income*tax_rate
-print(my_taxes)
+# my_income = 100
+# tax_rate = 0.1 # 10%
+# my_taxes = my_income*tax_rate
+# print(my_taxes)
 
-my_string = "abcdefg"
-print(my_string[-2])
-print(my_string[2:]) #slicing
-print(my_string[:2])
-print(my_string[1:6])
+# my_string = "abcdefg"
+# print(my_string[-2])
+# print(my_string[2:]) #slicing
+# print(my_string[:2])
+# print(my_string[1:6])
 
-print(my_string[:])
-print(my_string[::1])
-print(my_string[::2])
+# print(my_string[:])
+# print(my_string[::1])
+# print(my_string[::2])
 
-x = my_string.upper()
-print(x)
+# x = my_string.upper()
+# print(x)
 
-y = my_string.lower()
-print(y)
+# y = my_string.lower()
+# print(y)
 
-z = my_string.capitalize()
-print(z)
+# z = my_string.capitalize()
+# print(z)
 
-x = "My Name is vaibhav gupta i am spliting"
-print(x.split())
-print(x.split("a"))
+# x = "My Name is vaibhav gupta i am spliting"
+# print(x.split())
+# print(x.split("a"))
 
-x = "Insert anothr string here: {}".format("insert me!")
-print(x)
+# x = "Insert anothr string here: {}".format("insert me!")
+# print(x)
 
-x = "Item one: {} Item Two: {}".format('dog','cat')
-print(x)
+# x = "Item one: {} Item Two: {}".format('dog','cat')
+# print(x)
 
-x = "Item one: {y} Item Two: {x}".format(x = 'dog',y = 'cat')
-print(x)
+# x = "Item one: {y} Item Two: {x}".format(x = 'dog',y = 'cat')
+# print(x)
+
+mylist = [1,2,3]
+anotherlist = ["Vaibhav",1,2,3,4,23,1.5,True,"asdf",[1,2,3]]
+
+print(mylist)
+print(anotherlist)
+print(len(anotherlist))
+print(anotherlist[3:])
+print(anotherlist[:3])
+anotherlist[0] = 'Pooja'
+print(anotherlist)
+mylist.append('Hello')
+print(mylist)
+mylist.extend(['a','b','c'])
+print(mylist)
+lastitem = mylist.pop(2)
+print(mylist)
+print(lastitem)
+mylist.reverse()
+print(mylist)
+seclist = [32,23,45,12,1,0]
+seclist.sort()
+print(seclist)
+anotherlist = ["Vaibhav",1,2,3,4,23,1.5,True,"asdf",[1,2,3]]
+print(anotherlist[9])
+print(anotherlist[9][2])
+
+twoDarray = [[1,2,3],[4,5,6],[7,8,9]]
+#LIST COMPREHENSION
+first_col = [row[0] for row in twoDarray]
+print(first_col)
