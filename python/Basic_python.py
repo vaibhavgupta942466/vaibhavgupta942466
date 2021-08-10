56f9dd00ac908b5859b9256f69e1beb1127456cf
vaibhavgupta942466 <vaibhavgupta942466@gmail.com>
2021-08-10 22:40:54 +0530

    Rename

--- python/Basic_python.py
+++ python/Basic_python.py
@@ -42,33 +42,44 @@
 # x = "Item one: {y} Item Two: {x}".format(x = 'dog',y = 'cat')
 # print(x)
 
-mylist = [1,2,3]
-anotherlist = ["Vaibhav",1,2,3,4,23,1.5,True,"asdf",[1,2,3]]
+# mylist = [1,2,3]
+# anotherlist = ["Vaibhav",1,2,3,4,23,1.5,True,"asdf",[1,2,3]]
 
-print(mylist)
-print(anotherlist)
-print(len(anotherlist))
-print(anotherlist[3:])
-print(anotherlist[:3])
-anotherlist[0] = 'Pooja'
-print(anotherlist)
-mylist.append('Hello')
-print(mylist)
-mylist.extend(['a','b','c'])
-print(mylist)
-lastitem = mylist.pop(2)
-print(mylist)
-print(lastitem)
-mylist.reverse()
-print(mylist)
-seclist = [32,23,45,12,1,0]
-seclist.sort()
-print(seclist)
-anotherlist = ["Vaibhav",1,2,3,4,23,1.5,True,"asdf",[1,2,3]]
-print(anotherlist[9])
-print(anotherlist[9][2])
+# print(mylist)
+# print(anotherlist)
+# print(len(anotherlist))
+# print(anotherlist[3:])
+# print(anotherlist[:3])
+# anotherlist[0] = 'Pooja'
+# print(anotherlist)
+# mylist.append('Hello')
+# print(mylist)
+# mylist.extend(['a','b','c'])
+# print(mylist)
+# lastitem = mylist.pop(2)
+# print(mylist)
+# print(lastitem)
+# mylist.reverse()
+# print(mylist)
+# seclist = [32,23,45,12,1,0]
+# seclist.sort()
+# print(seclist)
+# anotherlist = ["Vaibhav",1,2,3,4,23,1.5,True,"asdf",[1,2,3]]
+# print(anotherlist[9])
+# print(anotherlist[9][2])
 
-twoDarray = [[1,2,3],[4,5,6],[7,8,9]]
-#LIST COMPREHENSION
-first_col = [row[0] for row in twoDarray]
-print(first_col)
+# twoDarray = [[1,2,3],[4,5,6],[7,8,9]]
+# #LIST COMPREHENSION
+# first_col = [row[0] for row in twoDarray]
+# print(first_col)
+
+my_stuff = {
+    "key1": "Value",
+    "key2": "VAlue2",
+    "key3": {"key4": [1,2,3]}
+}
+print(my_stuff["key2"])
+my_stuff["key2"] = "2nd Value"
+print(my_stuff["key2"])
+print(my_stuff)
+print(my_stuff["key3"]['key4'][2])
